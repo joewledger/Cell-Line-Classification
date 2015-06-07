cecb68f2d399ebbe88b313717721fc5ff4602602
joewledger <joewledger@gmail.com>
2015-06-07 12:43:34 -0400

    Project to classify cell lines using SVMs and Neural Networks with Mutation and Gene Expression Data as features and IC50 drug data as classifiers
    Signed-off-by: joewledger <joewledger@gmail.com>

---
+++ DataFormatting.py
@@ -0,0 +1,144 @@
+#File for creating and visualizing feature matrices
+#There are two types of matrices we will be working with
+#	1) Oncomap Mutation x Cell Line matrices
+#	2) Gene Expression x Cell Line matrices
+#All data sources for now are from CCLE
+
+#Format for all methods that return matrices
+#	All methods will return a tuple containing the following entries
+#	1) The matrix
+#	2) The list of names that correspond to the row entries
+#	3) The list of names that correspond to the column entries
+#	4) (Optional) The matrix of z-scores that correspond to the matrix
+
+import numpy as np
+
+#Returns a tuple containing
+#	1) A matrix of cell lines x genes
+#	2) A list of cell line names
+#	3) A list of gene names
+#	4) A matrix of z-scores
+#Parameter: filter_cells - boolean that tells us whether or not to filter out cell lines not in ic_50 data,
+#			or all cell lines that are not in both the IC50 and mutation data
+#	0 -- do nothing, 1 -- filter ic_50 only, 2 -- filter both
+def generate_cell_line_expression_matrix(filter_cells):
+	genes = list(row.split("\t")[0].strip() for row in open(expression_features_filename).readlines())
+	genes = filter(lambda entry: (entry != "" and entry[0].isupper()), genes)[1:len(genes)]
+	cell_lines = list(entry for entry in open(expression_features_filename).readline().split("\t") if entry.find("_") > 0)
+	if(filter_cells == 1): cell_lines = filter(lambda a: a[0:a.find("_")] in generate_ic_50_dict().keys(), cell_lines)
+	if(filter_cells == 2): cell_lines = filter(lambda a: a[0:a.find("_")] in trim_dicts(generate_ic_50_dict(),generate_mutation_dict())[0].keys(), cell_lines)
+	row_count = sum(1 for row in open(expression_features_filename))
+	col_count = sum(1 for col in open(expression_features_filename).readline().split("\t"))
+	m = np.zeros((len(genes),len(cell_lines)))
+	with open(expression_features_filename,'rb') as f:
+		lines = f.readlines()
+		cell_iden = lines[0].split("\t")
+		row,col = (0,0)
+		gene_num,cell_num = (0,0)
+		while(row < row_count):
+			col,cell_num = (0,0)
+			gene_vector = lines[row].split("\t")
+			if(gene_num + 1 > len(genes)): break
+			if(gene_vector[0] == genes[gene_num]):
+				while(col < col_count):
+					if(cell_num + 1 > len(cell_lines)): break
+					if(cell_iden[col] == cell_lines[cell_num]):
+						if(len(gene_vector) > col):
+							m[gene_num][cell_num] = gene_vector[col]
+						cell_num += 1
+					col += 1
+				gene_num += 1
+			row += 1
+	z_scores = np.matrix([sp.zscore(row) for row in m])
+	m = np.matrix(m).transpose()
+	z_scores = z_scores.transpose()
+	return tuple([m,cell_lines,genes,z_scores])
+
+#Returns a tuple containing
+#	1) A matrix of cell lines x Oncomap mutations
+#	2) A list of cell line names
+#	3) A list of mutations
+#Parameters: mutation_dict is a dictionary with keys as cell lines and entries as mutations on that cell line
+def generate_cell_line_mutation_matrix(mutation_dict):
+	cell_line_names = set()
+	all_mutations = set()
+	for key in mutation_dict.keys():
+		cell_line_names.add(key)
+	for value in mutation_dict.values():
+		for mutation in value:
+			all_mutations.add(mutation)
+	cell_line_names = sorted(list(cell_line_names))
+	all_mutations = (sorted(list(all_mutations)))
+	matrix = []
+	for cell_line in cell_line_names:
+		row = [0] * len(all_mutations)
+		for index,mutation in enumerate(all_mutations):
+			if (mutation in mutation_dict[cell_line]): row[index] = 1 
+		matrix.append(row)
+	return tuple([np.matrix(matrix),cell_line_names,all_mutations])
+
+#creates a dictionary that maps cell line names to ic_50 values
+def generate_ic_50_dict():
+	ic_50_dict = {}
+	with open(ic_50_filename,'rb') as ic_50_values:
+		for row_num,row in enumerate(ic_50_values):
+			fields = row.split("\t")
+			if(row_num > 0): ic_50_dict[str(fields[0])] = float(fields[1])
+	return ic_50_dict
+
+#Maps a cell line name to the set of mutations associated with it in the CCLE dataset
+#Returns a dictionary that maps each cell_line to a list of tuples
+#	Each tuple contains a mutated gene and its corresponding mutated protein
+def generate_mutation_dict():
+	mutation_dict = {}
+	with open(mutation_features_filename,'rb') as mutations:
+		for row_num,row in enumerate(mutations):
+			if(row_num == 0): continue
+			fields = row.split("\t")
+			cell_line = fields[15].strip()[0:fields[15].find("_")]
+			gene_name = fields[0].strip()
+			protein_change = fields[32].strip()
+			if(cell_line in mutation_dict):
+				mutation_dict[cell_line].extend([tuple([gene_name,protein_change])])
+			else:
+				mutation_dict[cell_line] = [tuple([gene_name,protein_change])]
+	return mutation_dict
+
+#Trims both dictionary parameters so that they contain the same set of keys
+#Returns a tuple containing the two dictionaries
+#Example usage: dictionaries = trim_dicts(mutation_dict,ic_50_dict) ; mutation_dict = dictionaries[0], ic_50_dict = dictionaries[1]
+def trim_dicts(dict1,dict2):
+	trimmed_dict_1 = dict1.copy()
+	trimmed_dict_2 = dict2.copy()
+	key_set_1 = dict1.keys()
+	key_set_2 = dict2.keys()
+	for key in key_set_1:
+		if (not key in key_set_2): trimmed_dict_1.pop(key,None)
+	for key in key_set_2:
+		if (not key in key_set_1): trimmed_dict_2.pop(key,None)
+	return tuple([trimmed_dict_1,trimmed_dict_2])
+
+def visualize_all():
+	mutation_dict = generate_mutation_dict()
+	trimmed_mutation_dict = trim_dicts(mutation_dict, generate_ic_50_dict())[0]
+
+	oncomap_matrix = generate_cell_line_mutation_matrix(mutation_dict)[0]
+	visualize_matrix(oncomap_matrix, "Oncomap Cell Line Mutation Matrix (All)" + str(oncomap_matrix.shape),"Mutations","Cell Lines", "Visualizations/Oncomap_Cell_Line_Mutations_Matrix.png")
+
+	oncomap_with_ic50_matrix = generate_cell_line_mutation_matrix(trimmed_mutation_dict)[0]
+	visualize_matrix(oncomap_with_ic50_matrix, "Oncomap Cell Line Mutation Matrix (only with IC50 values) " + str(oncomap_with_ic50_matrix.shape), "Mutations","Cell Lines", "Visualizations/Oncomap_Cell_Line_Mutations_Matrix_IC50.png")
+
+	expression_matrix = generate_cell_line_expression_matrix(0)[0]
+	visualize_matrix(expression_matrix, "Gene Expression Matrix" + str(expression_matrix.shape),"Genes","Cell Lines", "Visualizations/Gene_Expression_Matrix.png") 
+
+	expression_matrix_ic50 = generate_cell_line_expression_matrix(1)[0]
+	visualize_matrix(expression_matrix_ic50, "Gene Expression Matrix (Only with IC50 values)" + str(expression_matrix_ic50.shape),"Genes","Cell Lines", "Visualizations/Gene_Expression_Matrix_IC50.png")
+
+	expression_matrix_ic50_mutation = generate_cell_line_expression_matrix(2)[0]
+	visualize_matrix(expression_matrix_ic50_mutation, "Gene Expression Matrix (Only with IC50 values and mutation data)" + str(expression_matrix_ic50_mutation.shape),"Genes","Cell Lines", "Visualizations/Gene_Expression_Matrix_IC50_mutation.png")
+
+ic_50_filename = "CL_Sensitivity.txt"
+expression_features_filename = "CCLE_Data/CCLE_Expression_2012-09-29.res"
+mutation_features_filename = "CCLE_Data/CCLE_Oncomap3_2012-04-09.maf"
+
+visualize_all()
---
+++ SVM_Classification.py
@@ -0,0 +1,95 @@
+##---Cell Line Classification Project using SVM and Neural Networks
+##---Working with Elena Svenson under the direction of Dr. Mehmet Koyuturk
+##---We have IC50 values for a bunch of different cell lines for the drug we are testing(What is the drug called?)
+##---We are going to apply SVM to classify cell lines as either sensitive or resistant to this drug
+##---The training input values are the gene expression measurements for each cell line
+##---The training output values are the IC50 values discretized into several bins: "sensitive", "undetermined" , and "resistant"  (not sure about undetermined)
+
+from sklearn import *
+import numpy as np
+from matplotlib.pylab import *
+from matplotlib import colors,pyplot
+import scipy.stats.mstats as sp
+import DataFormatting as df
+
+#This method will generate a SVM classifier
+#Parameters: training subset - a list of the cell_line names that we will use to get features from, as well as IC50 values
+#Parameters: z_threshold - a value that will determine which features we use based on z-score
+def generate_svm_model(training_subset):
+	training_data = create_training_data(training_subset)
+	model = svm.SVC()
+	model.fit(training_data[0],training_data[1])
+	return model
+
+#This method will return a tuple containing the training input and output for our SVM classifier based on a list of cell_line names
+#Parameters: training subset - a list of the cell_line names that we will use to get features from, as well as IC50 values
+#Parameters: z_threshold - a value that will determine which features we use based on z-score
+#Output: a tuple, first entry is training input, second is training output
+#	Example for training input --- [[0,0],[1,1]], there are two samples, each with two features
+#	Example for training output -- [0,1], there are two samples, each with a classification in range (0, n_classes - 1)
+def create_training_data(training_subset):
+	training_input = []
+	training_output = []
+	for cell_line in training_subset:
+		cell_line_data = generate_cell_line_data(cell_line)
+		training_input.append(cell_line_data[0])
+		training_output.append(cell_line_data[1])
+	return tuple([training_input,training_output])
+
+#This method will return a tuple containing the feature inputs and output for a particular cell line
+#The first entry in the tuple will be the feature inputs for the cell_line
+#	This will consist of an array of the values for each of the features
+#The second entry in the tuple will be the training output for the sample
+#	This will be a value that determines which class the cell line is in ("sensitive", "undetermined", "resistant" AKA SUR)
+#Parameters: cell_line is the name of the cell_line we are interested in
+#Parameters: z_threshold is the z-score that will determine which features are actually selected
+#Parameters: class_bin is a function that will convert a numeric IC50 value into one of three classes
+#	The function should take an IC50 value and convert it to a number from 0-2 (corresponds to SUR)
+def generate_cell_line_data(cell_line):
+	feature_inputs = read_feature_inputs(cell_line)
+	ic_50 = generate_ic_50_dict()[cell_line]
+	class_bin = generate_class_bin()
+	classifier = [class_bin(ic_50)]
+	return tuple([feature_inputs, classifier])
+
+#Does n-fold cross-validation on our SVM model.
+#Saves these predictions along with the actual outputs in a tuple
+#	First entry in the tuple will be the actual output
+#	Second entry in the tuple will be the predicted output
+#Parameters: cell_lines - list of cell lines from IC_50 Data
+#Parameters: z_threshold - a value that will determine which features we use based on z-score
+#Parameters: num_folds - the number of folds we will use in the cross-fold validation (usually 5)
+def cross_validate_make_predictions(cell_lines,num_folds):
+	predictions = tuple([[],[]])
+	num_samples = len(cell_lines)
+	for fold in range(0,num_folds - 1):
+		lower_bound = int(float(fold) / float(num_folds) * float(num_samples))
+		upper_bound = int(float(fold + 1) / float(num_folds) * float(num_samples))
+		testing_cell_lines = cell_lines[lower_bound:upper_bound]
+		training_cell_lines = cell_lines[0:lower_bound].extend(cell_lines[upper_bound:len(cell_lines) - 1])
+		model = generate_svm_model(training_cell_lines)
+		for cell_line in testing_cell_lines:
+			cell_line_data = generate_cell_line_data(cell_line)
+			predictions[0].append(cell_line_data[1])
+			predictions[1].append(model.predict(cell_line[0]))
+	return predictions
+
+#Returns a function that classifies cell lines as either sensitive or resistant
+#Looks at distribution of IC50 values
+#	Top 15 Percent are Resistant -- marked 2
+#	Bottom 15 Percent are Senstive -- marked 0
+#	Rest are neutral -- marked 1
+def generate_class_bin():
+	ic_50_distribution = sorted(df.generate_ic_50_dict().values())
+	lower_bound = ic_50_distribution[int(float(len(ic_50_distribution)) * 3.0 / 20.0)]
+	upper_bound = ic_50_distribution[int(float(len(ic_50_distribution)) * 17.0 / 20.0)]
+	return lambda score: 0 if score < lower_bound else (2 if score > upper_bound else 1)
+
+#Returns a vector in list form of the values for each of the features of a cell line
+#For now we will only be doing OncoMap, eventually we should add parameters to allow for both OncoMap and Gene Expression data
+#For OncoMap, each feature is binary. Either mutation exists or mutation does not exist
+def read_feature_inputs(cell_line):
+	return "In progress"
+
+#def cross_validate_evalutate_predictions():
+
