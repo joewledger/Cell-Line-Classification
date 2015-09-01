94e5c8ac5b106962fe157a08ffaeb6edecc7411c
joewledger <joewledger@gmail.com>
2015-09-01 16:02:35 -0400

    Deleted code that dealt with mutations.
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Src/DataFormatting.py
+++ Src/DataFormatting.py
@@ -57,32 +57,6 @@ def normalize_expression_matrix(matrix):
 def strip_cell_lines_without_ic50(data_matrix):
     return data_matrix.drop(labels=[x for x in data_matrix.columns if x not in generate_ic_50_dict().keys()],axis=1)
 
-def generate_cell_line_mutation_matrix(mutation_dict):
-    """
-    Returns a tuple containing
-        1) A matrix of cell lines x Oncomap mutations
-        2) A list of cell line names
-        3) A list of mutations
-
-    :param mutation_dict: a dictionary with keys as cell lines and entries as mutations on that cell line
-    """
-    cell_line_names = set()
-    all_mutations = set()
-    for key in mutation_dict.keys():
-        cell_line_names.add(key)
-    for value in mutation_dict.values():
-        for mutation in value:
-            all_mutations.add(mutation)
-    cell_line_names = sorted(list(cell_line_names))
-    all_mutations = (sorted(list(all_mutations)))
-    matrix = []
-    for cell_line in cell_line_names:
-        row = [0] * len(all_mutations)
-        for index,mutation in enumerate(all_mutations):
-            if (mutation in mutation_dict[cell_line]): row[index] = 1
-        matrix.append(row)
-    return tuple([np.matrix(matrix),cell_line_names,all_mutations])
-
 def generate_ic_50_dict(ic_50_filename):
     """creates a dictionary that maps cell line names to ic_50 values"""
     ic_50_dict = {}
@@ -91,46 +65,3 @@ def generate_ic_50_dict(ic_50_filename):
             fields = row.split()
             if(row_num > 0): ic_50_dict[str(fields[0])] = float(fields[1])
     return ic_50_dict
-
-
-def generate_mutation_dict(mutation_features_filename):
-    """
-    Maps a cell line name to the set of mutations associated with it in the CCLE dataset
-    Returns a dictionary that maps each cell_line to a list of tuples
-        Each tuple contains a mutated gene and its corresponding mutated protein
-    """
-    mutation_dict = {}
-    with open(mutation_features_filename,'rb') as mutations:
-        for row_num,row in enumerate(mutations):
-            if(row_num == 0): continue
-            fields = row.split("\t")
-            cell_line = fields[15].strip()[0:fields[15].find("_")]
-            gene_name = fields[0].strip()
-            protein_change = fields[32].strip()
-            if(cell_line in mutation_dict):
-                mutation_dict[cell_line].extend([tuple([gene_name,protein_change])])
-            else:
-                mutation_dict[cell_line] = [tuple([gene_name,protein_change])]
-    return mutation_dict
-
-
-def trim_dicts(dict1,dict2):
-    """
-    Trims both dictionary parameters so that they contain the same set of keys
-    Returns a tuple containing the two dictionaries
-    Example usage: dictionaries = trim_dicts(mutation_dict,ic_50_dict) ; mutation_dict = dictionaries[0], ic_50_dict = dictionaries[1]"""
-    trimmed_dict_1 = dict1.copy()
-    trimmed_dict_2 = dict2.copy()
-    key_set_1 = dict1.keys()
-    key_set_2 = dict2.keys()
-    for key in key_set_1:
-        if (not key in key_set_2): trimmed_dict_1.pop(key,None)
-    for key in key_set_2:
-        if (not key in key_set_1): trimmed_dict_2.pop(key,None)
-    return tuple([trimmed_dict_1,trimmed_dict_2])
-
-def trim_dict(dictionary,array):
-    trimmed_dict = dictionary.copy()
-    for key in list(trimmed_dict.keys()):
-        if(not key in array): trimmed_dict.pop(key,None)
-    return trimmed_dict
