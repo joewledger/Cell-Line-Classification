a50d0b46cca53b6b22dbf2dddc488d2b554b1586
joewledger <joewledger@gmail.com>
2015-09-03 21:43:15 -0400

    Added functionality to save linear SVM coefficients.
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Src/Result_Writer.py
+++ Src/Result_Writer.py
@@ -15,7 +15,8 @@ ic50_filename = os.path.dirname(__file__) + '/../Data/IC_50_Data/CL_Sensitivity.
 def main():
     results_directory = get_results_filepath()
     make_results_directory_and_subdirectories(results_directory)
-    save_accuracy_threshold_graph(results_directory, expression_filename,ic50_filename,[float(x) * .01 for x in xrange(1,101)])
+    save_svm_accuracy_threshold_graph(results_directory, expression_filename,ic50_filename,[float(x) * .01 for x in xrange(1,101)])
+    save_svm_model_coefficients(results_directory,expression_filename,ic50_filename,[float(x) * .01 for x in xrange(1,4)])
 
 def get_results_filepath():
     """
@@ -38,6 +39,18 @@ def save_svm_accuracy_threshold_graph(results_directory,expression_file,ic50_fil
     outfile = results_directory + "Plots/accuracy_threshold.png"
     plt.plot_accuracy_threshold_curve(outfile,[x[0] for x in accuracies],[x[1] for x in accuracies])
 
+def save_svm_model_coefficients(results_directory, expression_file,ic50_file,thresholds):
+    results_file = results_directory + "Model_Coefficients/svm_linear.txt"
+    writer = open(results_file,"wb")
+    for threshold in thresholds:
+        writer.write("Threshold: %s\n" % str(threshold))
+        model = classify.construct_svc_model(kernel="linear")
+        expression_frame,ic50_series = dfm.generate_trimmed_thresholded_normalized_expression_frame(expression_file,ic50_filename,threshold)
+        genes = list(expression_frame.index)
+        model_coefficients = classify.get_svm_model_coefficients(model,expression_filename,ic50_filename,threshold)
+        writer.write(str([str(gene) for gene in genes]) + "\n")
+        writer.write(str([str(coef) for coef in model_coefficients]) + "\n\n")
+    writer.close()
 
 if __name__ == '__main__':
     main()
