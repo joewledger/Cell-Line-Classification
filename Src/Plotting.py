ac896d6da81717d80b07f807d57ede8abdced3ed
joewledger <joewledger@gmail.com>
2015-09-07 13:33:48 -0400

    Fixed legend on multiple kernels accuracy graph.
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Src/Plotting.py
+++ Src/Plotting.py
@@ -7,10 +7,16 @@ import matplotlib.pyplot as plt
 def plot_accuracy_threshold_multiple_kernels(outfile, kernels):
     thresholds = [x[0] for x in kernels[0]]
     kernel_labels={0:'linear',1:'rbf',2:'poly'}
+    kernel_colors={0:'red',1:'blue',2:'green'}
     plt.figure()
+    lines = [0] * 3
     for i,kernel in enumerate(kernels):
-        plt.plot(thresholds,[x[1][0] for x in kernel],label=kernel_labels[i])
-    plt.legend()
+        accuracy_scores = [x[1][0] for x in kernel]
+        error_bars = [x[1][1] for x in kernel]
+        lines[i], = plt.plot(thresholds,accuracy_scores,label=kernel_labels[i],color=kernel_colors[i])
+        plt.errorbar(thresholds,accuracy_scores,yerr=error_bars)
+
+    plt.legend([line for line in lines],['poly','linear','rbf'])
     plt.xlabel("Threshold")
     plt.ylabel("Accuracy")
     plt.title("Accuracy vs. Threshold Curve")
