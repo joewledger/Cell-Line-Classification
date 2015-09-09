46c909345f155c436d47e58c5c9cf1ba2399a8aa
joewledger <joewledger@gmail.com>
2015-09-09 17:20:25 -0400

    Made it easier to use full expression file from the command line.
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Src/Result_Writer.py
+++ Src/Result_Writer.py
@@ -25,7 +25,7 @@ def main():
     parser = argparse.ArgumentParser(description=description,formatter_class=RawTextHelpFormatter)
     parser.add_argument('--experiments',nargs='+',type=int,help='The experiments to run.')
     parser.add_argument('--results_dir',type=str,help='The directory to save results to.')
-    parser.add_argument('--expression_file',type=str,help='The file to read gene expression measurements from.')
+    parser.add_argument('--expression_file',type=str,help='The file to read gene expression measurements from (Use \'full\' for full dataset).')
     parser.add_argument('--ic50_file',type=str,help='The file to read the IC50 measurements from.')
     parser.add_argument('--patient_dir',type=str,help='The directory containing the patient information')
     parser.add_argument('--threshold_increment',type=float,help='The increment to test thresholds at.')
@@ -36,9 +36,10 @@ def main():
 
     results_directory = get_results_filepath(args.results_dir)
     make_results_directory_and_subdirectories(args.results_dir,results_directory)
+    expression_file = args.full_expression_file if args.expression_file == "full" else args.expression_file
 
     thresholds = [args.threshold_increment * x for x in xrange(1,args.num_thresholds + 1)]
-    run_experiments(results_directory,args.experiments,args.expression_file,args.ic50_file,args.patient_dir,thresholds,args.num_permutations)
+    run_experiments(results_directory,args.experiments,expression_file,args.ic50_file,args.patient_dir,thresholds,args.num_permutations)
 
 
 def run_experiments(results_directory, experiments, expression_filename,ic50_filename,patient_directory, thresholds,num_permutations):
@@ -55,6 +56,7 @@ def default_parameters():
     parameters['experiments'] = [x for x in xrange(0,5)]
     parameters['results_dir'] = os.path.dirname(__file__) + '/../Results/'
     parameters['expression_file'] = os.path.dirname(__file__) + '/../Data/CCLE_Data/sample1000.res'
+    parameters['full_expression_file'] = os.path.dirname(__file__) + '/../Data/CCLE_Data/CCLE_Expression_2012-09-29.res'
     parameters['ic50_file'] = os.path.dirname(__file__) + '/../Data/IC_50_Data/CL_Sensitivity.txt'
     parameters['patient_dir'] = os.path.dirname(__file__) + "Data/TCGA_Data/9f2c84a7-c887-4cb5-b6e5-d38b00d678b1/Expression-Genes/UNC__AgilentG4502A_07_3/Level_3"
     parameters['threshold_increment'] = .01
