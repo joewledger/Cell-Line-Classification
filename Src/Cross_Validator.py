08594c13a2bd51c017a5ceb5be6fb5f89f09f9ef
joewledger <joewledger@gmail.com>
2015-09-25 15:46:48 -0400

    Refactored Cross_Validator to consolidate both cross_val_score methods into one. Updated tests to relect change.
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Src/Cross_Validator.py
+++ Src/Cross_Validator.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import scipy.stats as stats
 from sklearn.externals.joblib import Parallel,delayed
 from sklearn.utils import check_arrays
 from sklearn.base import clone, is_classifier
@@ -9,6 +8,12 @@ from sklearn.cross_validation import check_cv
 from sklearn.cross_validation import _cross_val_score
 
 import DataFormatter as dfm
+
+
+
+#Example calls:
+#   cross_val_score_filter_feature_selection(model,trim_X_threshold,threshold,scikit_data,scikit_target,cv=5)
+#   cross_val_score_filter_feature_selection(model,trim_X_num_features,num_features,scikit_data,scikit_target,cv=5)
 
 def cross_val_score_filter_feature_selection(model,filter_function,filter_criteria, X, y=None, scoring=None, cv=None, n_jobs=1,
                     verbose=0, fit_params=None, score_func=None,
@@ -89,175 +94,6 @@ def cross_val_score_filter_feature_selection(model,filter_function,filter_criter
 
     scores = parallel(
         delayed(_cross_val_score)(clone(model), filter_function(X,y,train,filter_criteria), y, scorer, train, test,
-                                  verbose, fit_params)
-        for train, test in cv)
-    return np.array(scores)
-
-
-def cross_val_score_filter_feature_selection_feature_rank(estimator,num_features, X, y=None, scoring=None, cv=None, n_jobs=1,
-                    verbose=0, fit_params=None, score_func=None,
-                    pre_dispatch='2*n_jobs'):
-    """Evaluate a score by cross-validation
-
-    Parameters
-    ----------
-    estimator : estimator object implementing 'fit'
-        The object to use to fit the data.
-
-    X : array-like of shape at least 2D
-        The data to fit.
-
-    y : array-like, optional, default: None
-        The target variable to try to predict in the case of
-        supervised learning.
-
-    scoring : string, callable or None, optional, default: None
-        A string (see model evaluation documentation) or
-        a scorer callable object / function with signature
-        ``scorer(estimator, X, y)``.
-
-    cv : cross-validation generator, optional, default: None
-        A cross-validation generator. If None, a 3-fold cross
-        validation is used or 3-fold stratified cross-validation
-        when y is supplied and estimator is a classifier.
-
-    n_jobs : integer, optional
-        The number of CPUs to use to do the computation. -1 means
-        'all CPUs'.
-
-    verbose : integer, optional
-        The verbosity level.
-
-    fit_params : dict, optional
-        Parameters to pass to the fit method of the estimator.
-
-    pre_dispatch : int, or string, optional
-        Controls the number of jobs that get dispatched during parallel
-        execution. Reducing this number can be useful to avoid an
-        explosion of memory consumption when more jobs get dispatched
-        than CPUs can process. This parameter can be:
-
-            - None, in which case all the jobs are immediately
-              created and spawned. Use this for lightweight and
-              fast-running jobs, to avoid delays due to on-demand
-              spawning of the jobs
-
-            - An int, giving the exact number of total jobs that are
-              spawned
-
-            - A string, giving an expression as a function of n_jobs,
-              as in '2*n_jobs'
-
-    Returns
-    -------
-    scores : array of float, shape=(len(list(cv)),)
-        Array of scores of the estimator for each run of the cross validation.
-    """
-    X, y = check_arrays(X, y, sparse_format='csr', allow_lists=True)
-    cv = check_cv(cv, X, y, classifier=is_classifier(estimator))
-    scorer = _deprecate_loss_and_score_funcs(
-        loss_func=None,
-        score_func=score_func,
-        scoring=scoring
-    )
-    if scorer is None and not hasattr(estimator, 'score'):
-        raise TypeError(
-            "If no scoring is specified, the estimator passed "
-            "should have a 'score' method. The estimator %s "
-            "does not." % estimator)
-    # We clone the estimator to make sure that all the folds are
-    # independent, and that it is pickle-able.
-    fit_params = fit_params if fit_params is not None else {}
-    parallel = Parallel(n_jobs=n_jobs, verbose=verbose,
-                        pre_dispatch=pre_dispatch)
-
-    scores = parallel(
-        delayed(_cross_val_score)(clone(estimator), trim_X_num_features(X,y,train,num_features), y, scorer, train, test,
-                                  verbose, fit_params)
-        for train, test in cv)
-    return np.array(scores)
-
-
-def cross_val_score_filter_feature_selection_threshold(estimator,threshold, X, y=None, scoring=None, cv=None, n_jobs=1,
-                    verbose=0, fit_params=None, score_func=None,
-                    pre_dispatch='2*n_jobs'):
-    """Evaluate a score by cross-validation
-
-    Parameters
-    ----------
-    estimator : estimator object implementing 'fit'
-        The object to use to fit the data.
-
-    X : array-like of shape at least 2D
-        The data to fit.
-
-    y : array-like, optional, default: None
-        The target variable to try to predict in the case of
-        supervised learning.
-
-    scoring : string, callable or None, optional, default: None
-        A string (see model evaluation documentation) or
-        a scorer callable object / function with signature
-        ``scorer(estimator, X, y)``.
-
-    cv : cross-validation generator, optional, default: None
-        A cross-validation generator. If None, a 3-fold cross
-        validation is used or 3-fold stratified cross-validation
-        when y is supplied and estimator is a classifier.
-
-    n_jobs : integer, optional
-        The number of CPUs to use to do the computation. -1 means
-        'all CPUs'.
-
-    verbose : integer, optional
-        The verbosity level.
-
-    fit_params : dict, optional
-        Parameters to pass to the fit method of the estimator.
-
-    pre_dispatch : int, or string, optional
-        Controls the number of jobs that get dispatched during parallel
-        execution. Reducing this number can be useful to avoid an
-        explosion of memory consumption when more jobs get dispatched
-        than CPUs can process. This parameter can be:
-
-            - None, in which case all the jobs are immediately
-              created and spawned. Use this for lightweight and
-              fast-running jobs, to avoid delays due to on-demand
-              spawning of the jobs
-
-            - An int, giving the exact number of total jobs that are
-              spawned
-
-            - A string, giving an expression as a function of n_jobs,
-              as in '2*n_jobs'
-
-    Returns
-    -------
-    scores : array of float, shape=(len(list(cv)),)
-        Array of scores of the estimator for each run of the cross validation.
-    """
-    X, y = check_arrays(X, y, sparse_format='csr', allow_lists=True)
-    cv = check_cv(cv, X, y, classifier=is_classifier(estimator))
-    scorer = _deprecate_loss_and_score_funcs(
-        loss_func=None,
-        score_func=score_func,
-        scoring=scoring
-    )
-    if scorer is None and not hasattr(estimator, 'score'):
-        raise TypeError(
-            "If no scoring is specified, the estimator passed "
-            "should have a 'score' method. The estimator %s "
-            "does not." % estimator)
-    # We clone the estimator to make sure that all the folds are
-    # independent, and that it is pickle-able.
-    fit_params = fit_params if fit_params is not None else {}
-    parallel = Parallel(n_jobs=n_jobs, verbose=verbose,
-                        pre_dispatch=pre_dispatch)
-
-
-    scores = parallel(
-        delayed(_cross_val_score)(clone(estimator), trim_X_threshold(X,y,train,threshold), y, scorer, train, test,
                                   verbose, fit_params)
         for train, test in cv)
     return np.array(scores)
--- Tests/Test_Cross_Val.py
+++ Tests/Test_Cross_Val.py
@@ -13,32 +13,49 @@ def test_cross_val_score_filter_feature_selection_threshold():
     threshold = 1.0
     scikit_data,scikit_target = dfm.get_expression_scikit_data_target(expression_file, ic50_file,normalized=True,trimmed=True,threshold=None)
     model = classify.construct_svc_model(kernel='linear')
-    non_thresholded_test_1 = cv.cross_val_score_filter_feature_selection_threshold(model,threshold,scikit_data,scikit_target,cv=5)
+    non_thresholded_test_1 = cv.cross_val_score_filter_feature_selection(model,cv.trim_X_threshold,threshold,scikit_data,scikit_target,cv=5)
 
     m = classify.construct_svc_model(kernel='linear')
     s_data,s_target = dfm.get_expression_scikit_data_target(expression_file, ic50_file,normalized=True,trimmed=True,threshold=threshold)
     non_thresholded_test_2 = cross_val_score(m,s_data,s_target,cv=5)
 
-    #The non-thresholded tests should be the same because if we are not thresholding, it doesn't matter where we perform thresholding
-    assert(math.fabs(non_thresholded_test_1.mean() - non_thresholded_test_2.mean()) < .001)
-
     threshold = .05
     scikit_data,scikit_target = dfm.get_expression_scikit_data_target(expression_file, ic50_file,normalized=True,trimmed=True,threshold=None)
     model = classify.construct_svc_model(kernel='linear')
-    thresholded_test_1 = cv.cross_val_score_filter_feature_selection_threshold(model,threshold,scikit_data,scikit_target,cv=5)
+    thresholded_test_1 = cv.cross_val_score_filter_feature_selection(model,cv.trim_X_threshold,threshold,scikit_data,scikit_target,cv=5)
 
     m = classify.construct_svc_model(kernel='linear')
     s_data,s_target = dfm.get_expression_scikit_data_target(expression_file, ic50_file,normalized=True,trimmed=True,threshold=threshold)
     thresholded_test_2 = cross_val_score(m,s_data,s_target,cv=5)
 
+
+    #The non-thresholded tests should be the same because if we are not thresholding, it doesn't matter where we perform thresholding
+    assert(math.fabs(non_thresholded_test_1.mean() - non_thresholded_test_2.mean()) < .001)
+
     #The first non_thresholded test should have lower accuracy because we are doing thresholding within the cross-validation,
     #which will reduce cross-validation overfitting and as a consequence reported cross-validation accuracy.
-
     assert(thresholded_test_1.mean() - thresholded_test_2.mean() < 0)
 
 
 def test_cross_val_score_filter_feature_selection_feature_ranks():
+    #Calculate cross-val score for feature selection down to 10 features.
     num_features = 10
     scikit_data,scikit_target = dfm.get_expression_scikit_data_target(expression_file, ic50_file,normalized=True,trimmed=True,threshold=None)
     model = classify.construct_svc_model(kernel='linear')
-    print(cv.cross_val_score_filter_feature_selection_feature_rank(model,num_features,scikit_data,scikit_target,cv=5))
+    cv_score_10_features = cv.cross_val_score_filter_feature_selection(model,cv.trim_X_num_features,num_features,scikit_data,scikit_target,cv=5)
+
+    #Calculate cross-val score with no feature selection but using cv.cross_val_score_filter_feature_selection
+    scikit_data,scikit_target = dfm.get_expression_scikit_data_target(expression_file, ic50_file,normalized=True,trimmed=True,threshold=None)
+    num_features = len(scikit_data.tolist()[0])
+    model = classify.construct_svc_model(kernel='linear')
+    cv_score_all_features_1 = cv.cross_val_score_filter_feature_selection(model,cv.trim_X_num_features,num_features,scikit_data,scikit_target,cv=5)
+
+    scikit_data,scikit_target = dfm.get_expression_scikit_data_target(expression_file, ic50_file,normalized=True,trimmed=True,threshold=None)
+    model = classify.construct_svc_model(kernel='linear')
+    cv_score_all_features_2 = cross_val_score(model,scikit_data,scikit_target,cv=5)
+
+    #Feature selecction with only 10 features should have lower accuracy than feature selection with all features.
+    assert(cv_score_10_features.mean() - cv_score_all_features_1.mean() < 0)
+
+    #Feature selection with all features should have the same accuracy regardless of method of feature selection
+    assert(math.fabs(cv_score_all_features_1.mean() - cv_score_all_features_2.mean()) < .001)
